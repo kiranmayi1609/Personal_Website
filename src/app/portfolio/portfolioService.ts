13697703ced922b730cc704ce35ffd430676f429
Kiranmayi Tummala <kira1995@elev.tec.dk>
2023-09-20 14:47:24 +0200

    d 20.09

---
+++ src/app/portfolio/portfolioService.ts
@@ -0,0 +1,31 @@
+// import { Injectable } from '@angular/core';
+
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class PortfolioService {
+//   private portfolioItems = [
+//     {
+//       id: 1,
+//       title: 'Project 1',
+//       description: 'Description of Project 1 goes here.',
+//       imageUrl: 'assets/project1.jpg'
+//     },
+//     {
+//       id: 2,
+//       title: 'Project 2',
+//       description: 'Description of Project 2 goes here.',
+//       imageUrl: 'assets/project2.jpg'
+//     },
+//     {
+//       id: 3,
+//       title: 'Project 3',
+//       description: 'Description of Project 3 goes here.',
+//       imageUrl: 'assets/project3.jpg'
+//     }
+//   ];
+
+//   getPortfolioItems() {
+//     return this.portfolioItems;
+//   }
+// }
